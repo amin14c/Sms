a6cdc519b58f47a56fb3fca422936a67548b8d79
amin14c <amin14c@gmail.com>
2026-03-26 21:39:56 +0100

    Update SmsSenderreceiver.kt

--- App/Src/Main/Java/SmsSenderreceiver.kt
+++ App/Src/Main/Java/SmsSenderreceiver.kt
@@ -1,1 +1,18 @@
+package com.example.dailysms
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.SmsManager
+
+class SmsSenderReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val phone = intent.getStringExtra("phone")
+        val message = intent.getStringExtra("message")
+
+        if (phone != null && message != null) {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phone, null, message, null, null)
+        }
+    }
+}
